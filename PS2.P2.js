ef3cae92139afb0d1936fb6d1c067c82a3c9e8b2
Andrew Choi <dooby@bu.edu>
2022-10-04 10:55:37 -0400

    PS2.P2: string => str

--- PS2.P2.js
+++ PS2.P2.js
@@ -1,8 +1,8 @@
-function* sentenceGen (string){
-    const stringarray= string.split(" ")
+function* sentenceGen (str){
+    const strarray= str.split(" ")
 
-    for (let count = 0; count<stringarray.length; count++){
-        console.log(stringarray[count])
+    for (let count = 0; count<strarray.length; count++){
+        console.log(strarray[count])
     }
 }
 
