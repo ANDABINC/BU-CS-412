6d3c9bdb2ae0bf92eec976f6a13954962b6be3b2
Andrew Choi <dooby@bu.edu>
2022-10-05 04:08:59 -0400

    PS2.P2: sentenceGen reduced to one line.

--- PS2.P2.js
+++ PS2.P2.js
@@ -1,10 +1,15 @@
 function* sentenceGen (str){
-    const strarray= str.split(" ")
-
-    for (let count = 0; count<strarray.length; count++){
-        console.log(strarray[count])
-    }
+    yield* str.split(" ")
+    // const strarray= str.split(" ")  //yield* str.split(" ");
+    //
+    // for (let count = 0; count<strarray.length; count++){
+    //     console.log(strarray[count])
+    // }
 }
 
 const sentenceIter = sentenceGen("All I know is something like a bird within her sang")
-sentenceIter.next()
+let strnext = sentenceIter.next()
+while (strnext.done === false){
+    console.log(strnext.value)
+    strnext = sentenceIter.next()
+}
